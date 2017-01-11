e49e69dd53338e2c22727c629919d9e07455fc54
Paweł Skowroński <pkskowronski@gmail.com>
2017-01-11 23:02:32 +0100

    Added bootstrap theme

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,8 +3,52 @@ var React = require('react');
 var Main = React.createClass({
     render: function () {
         return (
-            <div className='main-container'>
-                {this.props.children}
+            <div>
+                <div className="brand">Kulinaria.pl</div>
+                <div className="address-bar">Wrocław</div>
+
+                <nav className="navbar navbar-default" role="navigation">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="index.html">Business Casual</a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li>
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+                                <li>
+                                    <a href="blog.html">Blog</a>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className='container'>
+                    {this.props.children}
+                </div>
+
+                <footer>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12 text-center">
+                                <p>Copyright &copy; Your Website 2014</p>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
         )
     }
